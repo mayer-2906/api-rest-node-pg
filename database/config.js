d40bf3e7ed54d1a3bfd6c1a54f74e85bb58b7587
jhony50769 <jhonathan.grisales@direktio.com>
2022-03-02 09:08:37 -0500

    Sprint 42. Conexion BD

--- database/config.js
+++ database/config.js
@@ -3,12 +3,12 @@ const { Client } = require('pg')
 
 const connectionData = {
     user: process.env.USER_DB,
-    host: 'localhost',
+    host: process.env.HST_DB, 
     database: process.env.DATABASE,
     password: process.env.PWS_DB,
     port: process.env.PORT_DB,
   }
-  //importe Poll en ves de client
+  
   const client = new Client(connectionData)
 
 module.exports = client;
