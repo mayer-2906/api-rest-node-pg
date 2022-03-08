8c955cc7ad01997d792ed45b2b848c61f10a8920
Mariela <mayercandelo.1985@gmail.com>
2022-03-08 14:20:29 -0500

    install pg-hstore

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@
 
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('dd37uik0okfu49', 'muqvkuenrwqqff', 'd7015551ee930119de94596eec604257b7f8220b3d4a1112dbfb7d65c0569d2f', {
+module.exports = new Sequelize(process.env.NAM_DB, 'muqvkuenrwqqff', 'd7015551ee930119de94596eec604257b7f8220b3d4a1112dbfb7d65c0569d2f', {
 
     host: 'ec2-54-156-110-139.compute-1.amazonaws.com',
     port: '5432',
